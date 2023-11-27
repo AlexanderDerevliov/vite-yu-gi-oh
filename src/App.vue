05988bbe9bde043877f17f64d0d73b28e4ddbcc4
AlexanderDerevliov <alex.feelred@gmail.com>
2023-11-27 18:03:23 +0100

    scaffolding with all installation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
-<script setup>
+<script>
+import axios from "axios";
+
+export default {
+  created(){
+    axios.get ("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0") 
+    .then ((resp) => {
+      console.log(resp)
+    });
+  }
+   
+  }
+
 
 </script>
 
 <template>
-<h1>hello</h1>
+<h1>Hello</h1>
 </template>
 
-<style >
+<style lang="scss" >
+@use "./style/general.scss"
 
 </style>
